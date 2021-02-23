31dd642be0fdceb1a41bb4a3eacb5ae63fdec508
Manasch <75514487+Manasch@users.noreply.github.com>
2021-02-23 18:24:21 +0530

    Third Commit

--- python-clock.py
+++ python-clock.py
@@ -13,13 +13,13 @@ wn.tracer(0)
 pen = tr.Turtle()
 pen.hideturtle()
 pen.speed(0)
-pen.pensize(2)
 
-def draw_circle(pen,r,fillcolor=False,col="#2F76C8"): #col - pencolor
+def draw_circle(pen,r,fillcolor=False,col="white"): #col - pencolor
     if fillcolor==True:
         pen.up()
         pen.goto(0,r)
         pen.setheading(180)
+        pen.pensize(2)
         pen.color(col)
         pen.pendown()
         pen.fillcolor(col)
@@ -34,6 +34,7 @@ def draw_circle(pen,r,fillcolor=False,col="#2F76C8"): #col - pencolor
         pen.up()
         pen.goto(0,r)
         pen.setheading(180)
+        pen.pensize(2)
         pen.color(col)
         pen.pendown()
         pen.circle(r)
@@ -42,12 +43,12 @@ def draw_circle(pen,r,fillcolor=False,col="#2F76C8"): #col - pencolor
         pen.goto(0,0)
         pen.setheading(90)
 
-def draw_line(pen,col="#2F76C8"):
+def draw_line(pen,col="white"):
 
     i=0
     while i < 12:
         pen.color(col)
-        pen.fd(180)
+        pen.fd(170)
         pen.pendown()
         pen.fd(20)
         pen.penup()
@@ -67,35 +68,47 @@ def draw_line(pen,col="#2F76C8"):
         j+=1
 
 
-def draw_hands(h, m, s, pen):
-    
 
-    pen.penup()
+def min_hand(pen,m):
+    
+    pen.up()
     pen.goto(0,0)
     pen.color("white")
+    pen.pensize(3)
     pen.setheading(90)
-    angle = (h/12)*360
+    angle=(m/60)*360
     pen.rt(angle)
     pen.pendown()
-    pen.fd(80)
+    pen.bk(2    0)
+    pen.fd(140)
 
-    pen.penup()
-    pen.goto(0,0)
-    pen.color("green")
-    pen.setheading(90)
-    angle = (m/60)*360
-    pen.rt(angle)
-    pen.pendown()
-    pen.fd(150)
+def sec_hand(pen,s):
 
-    pen.penup()
+    pen.up()
     pen.goto(0,0)
-    pen.color("yellow")
+    pen.color("red")
+    pen.pensize(5)
     pen.setheading(90)
-    angle = (s/60)*360
+    angle=(s/60)*360
     pen.rt(angle)
     pen.pendown()
-    pen.fd(30)
+    pen.bk(20)
+    pen.fd(160)
+
+def hour_hand(pen,h):
+
+    pen.up()
+    pen.goto(0,0)
+    pen.color("white")
+    pen.pensize(4)
+    pen.setheading(90)
+    angle=(h/12)*360
+    pen.rt(angle)
+    pen.pendown()
+    pen.bk(20)
+    pen.fd(100)
+
+
 
 #def print_num():
 
@@ -108,10 +121,12 @@ while True:
 
     draw_circle(pen,200)
     draw_circle(pen,220)
-    draw_circle(pen,20,True,"#58636f")
+    draw_circle(pen,5,True,"white")
     draw_line(pen)
-    draw_hands(h,m,s,pen)
-    
+    min_hand(pen,m)
+    sec_hand(pen,s)
+    hour_hand(pen,h)
+
     wn.update()
     time.sleep(1)
     pen.clear()
