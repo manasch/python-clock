90ebb5a6205be9c129d1e5c39638e23add1cb27d
Manasch <75514487+Manasch@users.noreply.github.com>
2021-02-28 23:19:43 +0530

    display date and time

--- python-clock.py
+++ python-clock.py
@@ -4,7 +4,7 @@ import tkinter as tk
 
 wn = tr.Screen()
 wn.bgcolor("black")
-wn.setup(width=1000, height=600)
+wn.setup(width=1200, height=800)
 wn.title("Analogue Clock")
 wn.tracer(0)
 
@@ -110,39 +110,48 @@ def print_num():
         pen.goto(0,-20)
         pen.setheading(deg)
         pen.up()
-        pen.fd(150)
+        pen.fd(145)
         pen.write(str(i),align="center",font=("Comic Sans MS",25,"normal"))
         pen.penup()
         deg+=30
         i-=1
 
-
-def onclick():
-
-    while True:
-        h=int(time.strftime("%I")) #returns the hour in 12 hour format
-        m=int(time.strftime("%M")) #returns the minute
-        s=int(time.strftime("%S")) #returns the second
-        h=(h*60 + m)/60
-
-        draw_circle(pen,200)
-        draw_circle(pen,220)
-        draw_circle(pen,5,True,"white")
-        draw_line(pen)
-        min_hand(pen,m)
-        sec_hand(pen,s)  
-        hour_hand(pen,h)
-        print_num()
-        
-
-        wn.update()
-        time.sleep(1)
-        pen.clear()
+def print_time(h,m,s):
+    pen.up()
+    pen.goto(0,240)
+    pen.write(f"{'%02d'%h}:{'%02d'%m}:{'%02d'%s} {time.strftime('%p')}",align="center",font=("Comic Sans MS",25,"normal"))
+    pen.up()
+    pen.goto(0,-290)
+    pen.write(f"{time.strftime('%d')}/{time.strftime('%b')}/{time.strftime('%Y')}, {time.strftime('%A')}",align="center",font=("Comic Sans MS",25,"normal"))
 
 
-canvas=wn.getcanvas()
-button=tk.Button(canvas.master, text="Click Here",command=onclick)
-canvas.create_window(-300,-200,window=button)
+#def onclick():
+
+while True:
+    h=int(time.strftime("%I")) #returns the hour in 12 hour format
+    m=int(time.strftime("%M")) #returns the minute
+    s=int(time.strftime("%S")) #returns the second
+    h1=(h*60 + m)/60
+
+    draw_circle(pen,200)
+    draw_circle(pen,220)
+    draw_circle(pen,5,True,"white")
+    draw_line(pen)
+    min_hand(pen,m)
+    sec_hand(pen,s)  
+    hour_hand(pen,h1)
+    print_num()
+    print_time(h,m,s)
+
+
+    wn.update()
+    time.sleep(1)
+    pen.clear()
+
+
+#canvas=wn.getcanvas()
+#button=tk.Button(canvas.master, text="Click Here",command=onclick)
+#canvas.create_window(-300,-200,window=button)
 
 
 wn.mainloop()
