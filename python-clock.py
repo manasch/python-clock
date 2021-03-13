f563645679a1206aad60c3965c88eeaf36272e28
Manasch <75514487+Manasch@users.noreply.github.com>
2021-03-13 12:17:50 +0530

    cleaned up code, v01

--- python-clock.py
+++ python-clock.py
@@ -1,7 +1,7 @@
 import turtle as tr
 import time
-import tkinter as tk
-from pytz import timezone
+#import tkinter as tk
+#from pytz import timezone
 
 wn = tr.Screen()
 wn.bgcolor("black")
@@ -10,10 +10,12 @@ wn.title("Analogue Clock")
 wn.tracer(0)
 
 pen = tr.Turtle()
-pen1 = tr.Turtle()
-pen1.hideturtle()
 pen.hideturtle()
 pen.speed(0)
+
+# second pen for stuff that doesn't change with time
+pen1 = tr.Turtle()
+pen1.hideturtle()
 pen1.speed(0)
 
 
@@ -59,7 +61,6 @@ length: length of the hand
 '''
 
 def draw_hands(pen,h_type,size,length,v=True,col="white"): # Set v as False to get the angle for hour, default is True for min & sec
-
     pen.up()
     pen.goto(0,0)
     pen.color(col)
@@ -75,11 +76,11 @@ def draw_hands(pen,h_type,size,length,v=True,col="white"): # Set v as False to g
     pen.bk(20)
     pen.fd(length)
 
-def print_num(pen):
+def print_num(pen,col="white"):
     deg=90
     i=12
     while i > 0:
-        pen.color("white")
+        pen.color(col)
         pen.up()
         pen.goto(0,-20)
         pen.setheading(deg)
@@ -104,8 +105,8 @@ def print_num(pen):
         i-=1
 '''
 
-def print_time(h,m,s):
-    pen.color('white')
+def print_time(h,m,s,col="white"):
+    pen.color(col)
     pen.up()
     pen.goto(0,240)
     pen.write(f"{'%02d'%h}:{'%02d'%m}:{'%02d'%s} {time.strftime('%p')}",align="center",font=("Comic Sans MS",25,"normal"))
@@ -113,19 +114,6 @@ def print_time(h,m,s):
     pen.goto(0,-290)
     pen.write(f"{time.strftime('%d')}/{time.strftime('%b')}/{time.strftime('%Y')}, {time.strftime('%A')}",align="center",font=("Comic Sans MS",25,"normal"))
 
-t_zones = [
-    "IST",
-    "UTC",
-]
-
-clicked=tk.StringVar()
-clicked.set(t_zones[0])
-
-canvas=wn.getcanvas()
-drop_down=tk.OptionMenu(canvas.master, clicked, *t_zones)
-canvas.create_window(-300,-200,window=drop_down)
-#canvas.mainloop()
-#def onclick():
 
 # drawing the inner circle
 draw_circle(pen1,200)
@@ -139,6 +127,7 @@ draw_circle(pen1,5,True)
 # drawing the stripes 
 draw_line(pen1)
 
+# printing the numbers of the clock
 print_num(pen1)
 
 # Infinite loop to keep the clock running until stopped
@@ -157,38 +146,14 @@ while True:
     h1=(h*60 + m)/60    
 
     #Drawing the hour, minute and second hand       
-    draw_hands(pen,h1,4,100,False) # hour
-    draw_hands(pen,m,2,140) # minute
-    draw_hands(pen,s,5,160,True,"red") # second
-    #print_num()
-    print_time(h,m,s)
+    draw_hands(pen,h1,4,100,False) # hour hand
+    draw_hands(pen,m,2,140) # minute hand
+    draw_hands(pen,s,5,160,True,"red") # second hand
+    print_time(h,m,s) # printing the digitial time and date
 
     wn.update()
     time.sleep(1)
     pen.clear()
 
-    '''clicked=tk.StringVar()
-    clicked.set(t_zones[0])
-
-    canvas=wn.getcanvas()
-    drop_down=tk.OptionMenu(canvas.master, clicked, *t_zones)
-    canvas.create_window(-300,-200,window=drop_down)
-    #canvas.mainloop()'''
-
-
-    #clicked=tk.StringVar()
-    #clicked.set(t_zones[0])
-
-'''canvas=wn.getcanvas()
-button=tk.Button(canvas.master, text="click here", command=onclick)
-#breakpoint()
-canvas.create_window(-300,-200,window=button)
-#canvas.mainloop()'''
-
-
-# mainloop() used to keep the screen running, closes instantly otherwise
+# mainloop() used to keep the screen running, closes instantly otherwise [unreachable code , outside while loop]
 #wn.mainloop()
-
-root = tk.Tkinter()
-button=Button(root, text="click here")
-root.mainloop()
