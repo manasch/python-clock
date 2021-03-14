9f4746c33a115d4447f7b215762c14c0cfdfd546
Manasch <75514487+Manasch@users.noreply.github.com>
2021-03-14 15:42:42 +0530

    menu in dev

--- python-clock.py
+++ python-clock.py
@@ -2,9 +2,11 @@ import turtle as tr
 import time
 import tkinter as tk
 from pytz import timezone
+import menu
+from menu import colors
 
 wn = tr.Screen()
-wn.bgcolor("black")
+wn.bgcolor(colors["bg_col"])
 wn.setup(width=1200, height=800)
 wn.title("Analogue Clock")
 wn.tracer(0)
@@ -17,7 +19,7 @@ pen.speed(0)
 pen1.speed(0)
 
 
-def draw_circle(pen,r,fillcolor=False,col="white",size=4): #col - pencolor
+def draw_circle(pen,r,col,fillcolor=False,size=4): #col - pencolor
     pen.up()
     pen.goto(0,r)
     pen.setheading(180)
@@ -31,7 +33,7 @@ def draw_circle(pen,r,fillcolor=False,col="white",size=4): #col - pencolor
     else:
         pen.circle(r)
 
-def draw_line(pen,col="white"):
+def draw_line(pen,col):
     pen.color(col)
     for i in range(60):
         psize = 2
@@ -58,7 +60,7 @@ size: width of the pen
 length: length of the hand
 '''
 
-def draw_hands(pen,h_type,size,length,v=True,col="white"): # Set v as False to get the angle for hour, default is True for min & sec
+def draw_hands(pen,h_type,size,length,col,v=True): # Set v as False to get the angle for hour, default is True for min & sec
 
     pen.up()
     pen.goto(0,0)
@@ -75,11 +77,11 @@ def draw_hands(pen,h_type,size,length,v=True,col="white"): # Set v as False to g
     pen.bk(20)
     pen.fd(length)
 
-def print_num(pen):
+def print_num(pen,col):
     deg=90
     i=12
     while i > 0:
-        pen.color("white")
+        pen.color(col)
         pen.up()
         pen.goto(0,-20)
         pen.setheading(deg)
@@ -88,21 +90,6 @@ def print_num(pen):
         pen.write(str(i),align="center",font=("Comic Sans MS",25,"normal"))
         deg+=30
         i-=1
-'''
-    # Can be enabled if want 24 hour format display on the clock
-    deg=120
-    i=23
-    while i > 12:
-        pen.up()
-        pen.goto(0,-15)
-        pen.setheading(deg)
-        pen.up()
-        pen.fd(110)
-        pen.write(str(i),align="center",font=("Comic Sans MS",15,"normal"))
-        pen.penup()
-        deg+=30
-        i-=1
-'''
 
 def print_time(h,m,s):
     pen.color('white')
@@ -113,6 +100,7 @@ def print_time(h,m,s):
     pen.goto(0,-290)
     pen.write(f"{time.strftime('%d')}/{time.strftime('%b')}/{time.strftime('%Y')}, {time.strftime('%A')}",align="center",font=("Comic Sans MS",25,"normal"))
 
+'''
 t_zones = [
     "IST",
     "UTC",
@@ -124,22 +112,27 @@ clicked.set(t_zones[0])
 canvas=wn.getcanvas()
 drop_down=tk.OptionMenu(canvas.master, clicked, *t_zones)
 canvas.create_window(-300,-200,window=drop_down)
-#canvas.mainloop()
-#def onclick():
+'''
 
 # drawing the inner circle
-draw_circle(pen1,200)
+draw_circle(pen1,200,col=colors["i_circle"])
 
 # drawing the outer circle
-draw_circle(pen1,220)
+draw_circle(pen1,220,col=colors["o_circle"])
 
 # drawing the gear in the center 
-draw_circle(pen1,5,True)
+draw_circle(pen1,5,fillcolor=True,col=colors["gear"])
 
 # drawing the stripes 
-draw_line(pen1)
+draw_line(pen1,col=colors["line"])
 
-print_num(pen1)
+print_num(pen1,col=colors["num"])
+
+canvas=wn.getcanvas()
+
+but=tk.Button(canvas.master, text="Customise",command=menu.menu)
+canvas.create_window(200, -200, window=but)
+#print(menu.colors)
 
 # Infinite loop to keep the clock running until stopped
 while True:
@@ -157,38 +150,16 @@ while True:
     h1=(h*60 + m)/60    
 
     #Drawing the hour, minute and second hand       
-    draw_hands(pen,h1,4,100,False) # hour
-    draw_hands(pen,m,2,140) # minute
-    draw_hands(pen,s,5,160,True,"red") # second
-    #print_num()
+    draw_hands(pen,h1,4,100,v=False,col=colors["hour"]) # hour
+    draw_hands(pen,m,2,140,col=colors["min"]) # minute
+    draw_hands(pen,s,5,160,v=True,col=colors["sec"]) # second
     print_time(h,m,s)
 
     wn.update()
     time.sleep(1)
     pen.clear()
 
-    '''clicked=tk.StringVar()
-    clicked.set(t_zones[0])
-
-    canvas=wn.getcanvas()
-    drop_down=tk.OptionMenu(canvas.master, clicked, *t_zones)
-    canvas.create_window(-300,-200,window=drop_down)
-    #canvas.mainloop()'''
-
-
-    #clicked=tk.StringVar()
-    #clicked.set(t_zones[0])
-
-'''canvas=wn.getcanvas()
-button=tk.Button(canvas.master, text="click here", command=onclick)
-#breakpoint()
-canvas.create_window(-300,-200,window=button)
-#canvas.mainloop()'''
-
 
 # mainloop() used to keep the screen running, closes instantly otherwise
-#wn.mainloop()
+wn.mainloop()
 
-root = tk.Tkinter()
-button=Button(root, text="click here")
-root.mainloop()
